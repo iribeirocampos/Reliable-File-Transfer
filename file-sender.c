20aac20f1b66523cca2e24942af0b54be0265687
iuri <iribeirocampos@gmail.com>
2024-12-13 12:05:20 +0000

    Cleaning prints

--- file-sender.c
+++ file-sender.c
@@ -74,7 +74,7 @@ int main(int argc, char *argv[])
     if (retry)
     {
       // setting to last chunk
-      printf("resetting cursor, to %d\n", ntohl(ack_pkt.seq_num));
+      // printf("resetting cursor, to %d\n", ntohl(ack_pkt.seq_num));
       // printf("S: RESENDING SEQ NUM %d\n", received_ack);
       fseek(file, ntohl(ack_pkt.seq_num) * MAX_CHUNK_SIZE, 0);
       // fseek(file, received_ack * MAX_CHUNK_SIZE, 0);
@@ -87,7 +87,7 @@ int main(int argc, char *argv[])
     // Load data from file.
 
     // Send segment.
-    printf("TESTE: Window position: %d, SENT: %d\n", window_position, sent);
+    // printf("TESTE: Window position: %d, SENT: %d\n", window_position, sent);
     if (!last_package || sent == 0)
     {
       while (sent < window_position + max_window_size)
@@ -98,7 +98,7 @@ int main(int argc, char *argv[])
             sendto(sockfd, &data_pkt, offsetof(data_pkt_t, data) + data_len, 0,
                    (struct sockaddr *)&srv_addr, sizeof(srv_addr));
         // printf("S: SENDING SEG,  %d\n", data_pkt.seq_num);
-        printf("S: Sending segment %d, size %ld.\n", ntohl(data_pkt.seq_num),
+        printf("SENDER: Sending segment %d, size %ld.\n", ntohl(data_pkt.seq_num),
                offsetof(data_pkt_t, data) + data_len);
         if (sent_len != offsetof(data_pkt_t, data) + data_len)
         {
@@ -108,7 +108,7 @@ int main(int argc, char *argv[])
         if (feof(file))
         {
           last_package = 1;
-          printf("END OF FILE\n");
+          // printf("END OF FILE\n");
         }
         sent++;
       }
@@ -117,21 +117,20 @@ int main(int argc, char *argv[])
     if (recvfrom(sockfd, &ack_pkt, sizeof(ack_pkt), 0,
                  (struct sockaddr *)&src_addr, &(socklen_t){sizeof(src_addr)}) < 0)
     {
-      printf("S: Timeout\n");
+      // printf("S: Timeout\n");
       num_timeouts += 1;
       retry = 1;
     }
     else
     {
-      printf("SENDER: ACK RECEIVED %d SEQ %d\n", ntohl(ack_pkt.seq_num), ntohl(data_pkt.seq_num));
+      // printf("SENDER: ACK RECEIVED %d SEQ %d\n", ntohl(ack_pkt.seq_num), ntohl(data_pkt.seq_num));
       if (ntohl(ack_pkt.seq_num) == received_ack)
       {
         dupack_counter++;
-
-        printf("S: DUPACK, number %d with SLECTIVE %d\n", dupack_counter, ntohl(ack_pkt.selective_acks));
+        printf("SENDER: DUPACK, number %d with SLECTIVE %d\n", dupack_counter, ntohl(ack_pkt.selective_acks));
         if (dupack_counter >= 3)
         {
-          printf("S: Fast retransmit.\n");
+          // printf("S: Fast retransmit.\n");
           retry = 1;
         }
         continue;
@@ -141,13 +140,13 @@ int main(int argc, char *argv[])
         received_ack = ntohl(ack_pkt.seq_num);
         dupack_counter = 0;
         window_position = ntohl(ack_pkt.seq_num);
-        printf("S: Window position: %d\n", window_position);
-        printf("S: Received ack %d.\n", ntohl(ack_pkt.seq_num));
+        // printf("S: Window position: %d\n", window_position);
+        // printf("S: Received ack %d.\n", ntohl(ack_pkt.seq_num));
       }
     }
     if (last_package && sent == received_ack)
     {
-      printf("LAST TEST: ENVIADO %d, ACK %d\n", sent, received_ack);
+      // printf("LAST TEST: ENVIADO %d, ACK %d\n", sent, received_ack);
       last_package = 0;
     }
   }
